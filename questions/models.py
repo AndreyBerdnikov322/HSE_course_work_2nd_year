cd379bf64f73c32c10ae3ecfa20ec4f23a644e12
AndreyBerdnikov322 <akberdnikov@edu.hse.ru>
2025-03-24 16:08:09 +0500

    initial Django project setup

--- questions/models.py
+++ questions/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Question(models.Model):
+    text = models.TextField("Текст вопроса")
+    image = models.ImageField("Изображение", upload_to="questions/", blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.text[:50]
